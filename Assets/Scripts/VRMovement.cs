88e84017e02a25ca78c40872153d10e9a7e34654
Florian Saling <florian@suedring54a.de>
2019-08-18 22:31:58 +0200

    UPD VRMovement | now possible to walk with different speeds

--- Assets/Scripts/VRMovement.cs
+++ Assets/Scripts/VRMovement.cs
@@ -9,8 +9,15 @@ public class VRMovement : MonoBehaviour
 
     private Vector3 lookDir;
     private bool m_isMoving;
-    public float speedMultiplier;
+    private bool timerEnabled = false;
+
+    private float timeSinceLastRecognition;
+    private float speedMultiplier;
+    private float speed;
+
+    public float baseSpeed;
     public float waitPeriod;
+
 
     public bool IsWalking { get; set; }
 
@@ -18,9 +25,12 @@ public class VRMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        speedMultiplier = 1.0f;
+
         if(waitPeriod == 0f)
         {
-            waitPeriod = 1.2f;
+            waitPeriod = 1f;
         }
 
         IsWalking = false;
@@ -34,35 +44,91 @@ public class VRMovement : MonoBehaviour
         //Set lookDir to the current direction the player / camera is looking at
         lookDir = transform.InverseTransformDirection(Camera.main.transform.forward);
 
-        //Mimic the kinect bool input
+        //The actual movement speed is a combined value of a base speed and a multiplier
+        //The multiplier is the calculated duration between two recognised gestures
+        speed = baseSpeed * speedMultiplier;
+
+
+        //Debug the Microsoft Kinect bool input
         if (Input.GetKeyDown(KeyCode.K))
         {
             IsWalking = true;
         }
 
+
+        if(timerEnabled == true)
+        {
+            timeSinceLastRecognition += Time.deltaTime;
+        }
+
+
         if (IsWalking == true)
         {
+            timerEnabled = true;
+
             //Waiting for more walk input from kinect 
             StopAllCoroutines();
             StartCoroutine("WaitForInput");
+
+            //Debug the speed variables
+            Debug.Log("Rectime: " + timeSinceLastRecognition + "Multiplier: " + speedMultiplier + "Walk Speed: " + speed);
+
+            //Map the speed variables to the player velocity range
+            if (timeSinceLastRecognition != 0)
+            {
+
+                //if (timeSinceLastRecognition > 0.7f)
+                //{
+                //    speedMultiplier = 0.5f;
+                //}
+
+                //else if (timeSinceLastRecognition > 0.5f && timeSinceLastRecognition < 0.7f)
+                //{
+                //    speedMultiplier = 1f;
+                //}
+
+                //else if (timeSinceLastRecognition < 0.4f)
+                //{
+                //    speedMultiplier = 2f;
+                //}
+
+                Mathf.Clamp(speedMultiplier = MapValue(timeSinceLastRecognition, waitPeriod, 0.5f, 0.7f, 1.8f), 0.7f, 2f);
+            }
+
+
+            timeSinceLastRecognition = 0;
+
             m_isMoving = true;
             IsWalking = false;
+            
         }
 
         if (m_isMoving == true)
         {
 
             //Walk in the X/Z direction of the current direction the player / camera is looking at
-            Player.transform.Translate(new Vector3(lookDir.x, 0, lookDir.z) * Time.deltaTime * speedMultiplier);
+            Player.transform.Translate(new Vector3(lookDir.x, 0, lookDir.z) * (Time.deltaTime * speed));
 
         }
+
     }
 
+    //Wait x amount of seconds then disable movement when coroutine is not restarted by additional movement
     IEnumerator WaitForInput()
     {
-        //Wait x seconds then disable movement when coroutine is not restarted by additional movement
+        
         yield return new WaitForSeconds(waitPeriod);
         m_isMoving = false;
+        timerEnabled = false;
+        timeSinceLastRecognition = 0;
+
+    }
+
+
+    //This is the mapping function to map a value from one range to another
+    public static float MapValue(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 
 }
